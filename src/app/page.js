032b262a3d5b82cf73b629d513e09341e5f8303e
Poulav Bhowmick <bpoulav@gmail.com>
2023-12-02 17:36:54 +0530

    functionalities 2.0

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,40 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsChevronLeft, BsChevronRight, BsPencil, BsTrash } from 'react-icons/bs';
-import data from "./data.json";
 
 function App() {
-  const [users, setUsers] = useState(data);
+  const [users, setUsers] = useState([]);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const [selectedUsers, setSelectedUsers] = useState([]);
-
   const [editingRow, setEditingRow] = useState(null);
 
-  const header = [" ", "Name", "Email", "Role", "Actions"];
+  const header = ["Name", "Email", "Role", "Actions"];
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []); 
 
   const filteredUsers = users.filter(user => {
     return (
       user.name.toLowerCase().includes(search.toLowerCase()) ||
-      user.email.toLowerCase().includes(search.toLowerCase())
+      user.email.toLowerCase().includes(search.toLowerCase()) ||
+      user.role.toLowerCase().includes(search.toLowerCase())
     );
   });
 
+  const totalPages = Math.ceil(filteredUsers.length / 10);
+  const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
   const displayUsers = filteredUsers.slice((page - 1) * 10, page * 10);
 
   function toggleUserSelection(user) {
@@ -32,8 +47,13 @@ function App() {
     });
   }
 
+  function toggleSelectAll() {
+    setSelectedUsers(prevSelected =>
+      prevSelected.length === displayUsers.length ? [] : [...displayUsers]
+    );
+  }
+
   function editUser(user, field, value) {
-    // Implement your edit logic here
     const updatedUsers = users.map(u =>
       u.id === user.id ? { ...u, [field]: value } : u
     );
@@ -41,153 +61,189 @@ function App() {
   }
 
   function saveEditing(user) {
-    // Implement your save editing logic here
     setEditingRow(null);
   }
 
   function deleteRow(user) {
-    // Implement your delete logic here
     const updatedUsers = users.filter(u => u.id !== user.id);
     setUsers(updatedUsers);
   }
 
-  const totalPages = Math.ceil(filteredUsers.length / 10);
-
-  const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
-
-  function gotoPage(page) {
-    setPage(page);
+  function deleteSelectedRows() {
+    const updatedUsers = users.filter(u => !selectedUsers.includes(u));
+    setUsers(updatedUsers);
+    setSelectedUsers([]);
   }
 
-  function prevPage() {
-    if (page > 1) {
-      setPage(page - 1);
-    }
+  function deleteAllRowsOnPage() {
+    const updatedUsers = users.filter(u => !displayUsers.includes(u));
+    setUsers(updatedUsers);
   }
 
-  function nextPage() {
-    if (page < totalPages) {
-      setPage(page + 1);
+  function gotoPage(newPage) {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
     }
   }
 
   return (
-    <div className="font-mono text-center flex h-full flex-col bg-gray-100 h-screen">
-
+    <div className="font-mono text-center flex flex-col bg-gray-100 min-h-screen">
       <input
         type="text"
-        placeholder="Start typing to Search"
+        placeholder="Search..."
         className="border rounded py-2 px-4 w-1/3 self-center mt-4 text-black"
         value={search}
         onChange={e => setSearch(e.target.value)}
       />
 
-      <div className="border overflow-x-auto rounded text-black my-32">
+      <div className="border overflow-x-auto rounded text-black my-32 relative">
+        <button
+          className="absolute top-0 right-0 m-4 px-4 py-2 bg-red-500 text-white rounded items-center justify-center flex"
+          onClick={deleteSelectedRows}
+          disabled={selectedUsers.length === 0}
+        >
+          <BsTrash className="mr-2" />
+          Delete Selected
+        </button>
+
         <table className="table-auto w-full my-4">
           <thead>
-            <tr className="bg-gray-300 ">
+            <tr className="bg-gray-300">
+              <th className="border p-2">
+                <input
+                  type="checkbox"
+                  onChange={toggleSelectAll}
+                  checked={selectedUsers.length === displayUsers.length}
+                />
+              </th>
               {header.map(title => (
                 <th className="border p-2" key={title}>
                   {title}
                 </th>
               ))}
+              <th className="border p-2">Actions</th>
             </tr>
           </thead>
           <tbody>
-  {displayUsers.map(user => (
-       <tr
-       className="hover:bg-gray-100 cursor-pointer py-4"
-       key={user.id}
-     >
-      <td>
-        <input
-          type="checkbox"
-          checked={selectedUsers.includes(user)}
-          onChange={() => toggleUserSelection(user)}
-        />
-      </td>
+            {displayUsers.map((user, index) => (
+              <tr
+                className={`${
+                  selectedUsers.includes(user) ? 'bg-gray-200' : 'hover:bg-gray-100'
+                } cursor-pointer py-4`}
+                key={user.id}
+              >
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedUsers.includes(user)}
+                    onChange={() => toggleUserSelection(user)}
+                  />
+                </td>
 
-      {user.id === editingRow ? (
-        <>
-          <td>
-            <input
-              value={user.name}
-              onChange={e => editUser(user, "name", e.target.value)}
-            />
-          </td>
-          <td>
-            <input
-              value={user.email}
-              onChange={e => editUser(user, "email", e.target.value)}
-            />
-          </td>
-          <td>
-            <select
-              value={user.role}
-              onChange={e => editUser(user, "role", e.target.value)}
-            >
-              <option value="member">Member</option>
-              <option value="admin">Admin</option>
-            </select>
-          </td>
-          <td>
-            <button onClick={() => saveEditing(user)}>Save</button>
-          </td>
-        </>
-      ) : (
-        <>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.role}</td>
-          <td>
-            <div className="flex">
-              <BsPencil
-                className="cursor-pointer text-blue-500 mr-2"
-                onClick={() => setEditingRow(user.id)}
-              />
-              <BsTrash
-                className="cursor-pointer text-red-500"
-                onClick={() => deleteRow(user)}
-              />
-            </div>
-          </td>
-        </>
-      )}
-    </tr>
-  ))}
-</tbody>
-
+                {user.id === editingRow ? (
+                  <>
+                    <td>
+                      <input
+                        value={user.name}
+                        onChange={e => editUser(user, "name", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        value={user.email}
+                        onChange={e => editUser(user, "email", e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <select
+                        value={user.role}
+                        onChange={e => editUser(user, "role", e.target.value)}
+                      >
+                        <option value="member">Member</option>
+                        <option value="admin">Admin</option>
+                      </select>
+                    </td>
+                    <td>
+                      <button className="save" onClick={() => saveEditing(user)}>
+                        Save
+                      </button>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                    <td>
+                      <div className="flex justify-around">
+                        <button
+                          className="edit"
+                          onClick={() => setEditingRow(user.id)}
+                        >
+                          <BsPencil />
+                        </button>
+                        <button
+                          className="delete"
+                          onClick={() => deleteRow(user)}
+                        >
+                          <BsTrash />
+                        </button>
+                      </div>
+                    </td>
+                  </>
+                )}
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
       <div className="fixed bottom-4 right-4 flex">
-
         <button
           className="h-8 w-8 rounded-full border bg-gray-300 text-black"
-          onClick={prevPage}
+          onClick={() => gotoPage(1)}
           disabled={page === 1}
         >
-          <BsChevronLeft className="m-1" />
+          {'<<'}
         </button>
-
+        <button
+          className="h-8 w-8 rounded-full border bg-gray-300 text-black mx-0.5"
+          onClick={() => gotoPage(page - 1)}
+          disabled={page === 1}
+        >
+          {'<'}
+        </button>
         {pageNumbers.map(num => (
           <button
             key={num}
             className={`h-8 w-8 rounded-full border mx-0.5 bg-gray-300 text-black 
-             flex items-center justify-center ${num === page && 'bg-blue-500 text-white'}`}
+              flex items-center justify-center ${num === page && 'bg-blue-500 text-white'}`}
             onClick={() => gotoPage(num)}
           >
             {num}
           </button>
         ))}
-
         <button
-          className="h-8 w-8 rounded-full border bg-gray-300 text-black"
-          onClick={nextPage}
+          className="h-8 w-8 rounded-full border bg-gray-300 text-black mx-0.5"
+          onClick={() => gotoPage(page + 1)}
           disabled={page === totalPages}
         >
-          <BsChevronRight className="m-1" />
+          {'>'}
         </button>
-
+        <button
+          className="h-8 w-8 rounded-full border bg-gray-300 text-black"
+          onClick={() => gotoPage(totalPages)}
+          disabled={page === totalPages}
+        >
+          {'>>'}
+        </button>
+      </div>
+      <div className="fixed bottom-4 left-4 flex items-center">
+        <button
+          className="h-8 w-8 rounded-full border bg-red-500 text-white items-center justify-center flex"
+          onClick={deleteAllRowsOnPage}
+        >
+          <BsTrash />
+        </button>
       </div>
     </div>
   );
